d24912006b9d37f4556e24f2ad1694f3cafb94af
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-02-10 14:50:19 +1300

    check name function

---
+++ 01_Base_Component.py
@@ -0,0 +1,25 @@
+# import statements
+
+# functions
+
+# main routine
+
+# ask if user has used the program befre
+
+# loop to get ticket details
+
+    # get name (deny blank)
+
+    # get age (between 12 and 130)
+
+    # calculate ticket price
+
+    # loop to ask for snacks
+
+    # calculate snack price
+
+    # ask for payment method (and apply surcharge if necessary)
+
+# calculate total sales and profit
+
+# output data to text file
---
+++ 02_check_name_v2.py
@@ -0,0 +1,13 @@
+def not_blank(question):
+    loop = True
+    while loop is not False:
+        response = input(question)
+        if response == "":
+            print("Sorry - this can't be blank")
+        else:
+            return response
+            loop = False
+            
+# Main routine
+
+name = not_blank("What's your name? ")
